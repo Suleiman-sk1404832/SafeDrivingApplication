1110d629bfc86b51170918d285a19e2393ed58a7
suleiman-sk1404832 <sk1404832@qu.edu.qa>
2018-02-27 17:46:38 +0300

    initial stuff

---
+++ app/src/main/java/qa/edu/qu/cmps312/safedrivingapplication/LoginFragment.java
@@ -0,0 +1,25 @@
+package qa.edu.qu.cmps312.safedrivingapplication;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class LoginFragment extends Fragment {
+
+
+    public LoginFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView =inflater.inflate(R.layout.activity_main,container,false);
+
+        return rootView;
+    }
+}
