e776d0728f4787de4a0831de65fd51241dd6558f
Mohammed-mm1303624 <mm1303624@qu.edu.qa>
2018-03-13 18:50:09 +0300

    did add car validations

--- app/src/main/java/qa/edu/qu/cmps312/safedrivingapplication/fragments/AddCarFragment.java
+++ app/src/main/java/qa/edu/qu/cmps312/safedrivingapplication/fragments/AddCarFragment.java
@@ -65,6 +65,11 @@ public class AddCarFragment extends Fragment {
                 String mYear = year.getText().toString();
                 String mMilage = milage.getText().toString();
                 if (isNotEmpty(mMake) && isNotEmpty(mModel) && isNotEmpty(mYear) && isNotEmpty(mMilage)) {
+                    if(Integer.parseInt(year.getText().toString())< 1980 || Integer.parseInt(year.getText().toString())> 2018)
+                        year.setError("the year should be between 1980 and 2018");
+                    else if(Integer.parseInt(milage.getText().toString())< 0 || Integer.parseInt(milage.getText().toString())> 300000)
+                        milage.setError("the mileage should be between 0 and 300000");
+                    else
                     addCarInterface.submitCar(mMake, mModel, mYear, mMilage);
                 } else
                     Toast.makeText(getContext(), "Please Fill all the fields", Toast.LENGTH_SHORT).show();
